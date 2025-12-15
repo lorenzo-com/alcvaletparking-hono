55d6bf0460fec620d439936d7b4987a02ae0b0b1
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-12-15 09:35:34 +0100

    fix nombre de dominio en cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use('/*', cors({
         'http://localhost:5173', // Svelte (Vite) en desarrollo
         'https://alcvaletparking.com', // dominio de producción (Astro)
         'https://admin.alcvaletparking.com', // dominio de admin (Svelte)
-        'https://alcvaletparking-hono.lorenzokrastev1000.workers.dev' // Dominio de cloudflare
+        'https://alc-valet-parking-astro.pages.dev' // Dominio de cloudflare
     ],
     allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type', 'Authorization', 'apikey', 'X-Client-Info'],
